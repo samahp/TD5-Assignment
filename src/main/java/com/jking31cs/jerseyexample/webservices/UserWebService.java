a613c9fa45b8713259472126862b30744cf44d67
samahp <sama@gatech.edu>
2015-10-26 20:10:10 -0400

    Modified UserWebService.java

--- src/main/java/com/jking31cs/jerseyexample/webservices/UserWebService.java
+++ src/main/java/com/jking31cs/jerseyexample/webservices/UserWebService.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -50,8 +51,20 @@ public class UserWebService {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public User updateList(@PathParam("id") Long id, User user) {
+        user.setId(id);
         return store.save(user);
     }
+
+
+    @DELETE
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public User delete(@PathParam("id") Long id) {
+        User user = store.get(id);
+        return store.delete(user);
+    }
+
 
 
 }
